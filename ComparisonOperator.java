21b17c4b06552508f2ce0afc8f28b5b223fcb070
Fikries <fikries184@gmail.com>
2025-01-25 17:09:41 +0800

    logical operator

---
+++ ComparisonOperator.java
@@ -0,0 +1,69 @@
+//ini contoh coding untuk lebih besar
+//output coding ini ialah false
+/* 
+public class ComparisonOperator{
+    public static void main(String[] args){
+        boolean x = true;
+        boolean y = false;
+        int a = 5, b = 10;
+
+        if (a > b) {
+            System.out.println(x);
+        }
+        else{
+            System.out.println(y);
+        }
+
+    }
+}
+    */
+
+//ini contoh coding untuk not equal
+/*ayat matematik yang mudah difahami ialah "adakah 5 tidak sama dengan 5"
+jawapan nya ialah TIDAK SAMA*/
+/* 
+public class ComparisonOperator{
+    public static void main(String[] args){
+        int x = 5, y = 5;
+        System.out.println(x!=y);
+
+        }
+    }
+ */
+
+
+ //ini merupakan contoh coding untuk logical operator(atau)
+//output coding ini ialah false
+/*
+ public class ComparisonOperator{
+    public static void main(String[] args){
+        int x =5, y =10;// variable yang dideclare
+        boolean z = true;
+        boolean c = false;
+
+        if(x>6 || y> 11){//logical operator atau "||"
+            System.out.println(z);
+        }// jika x dan y tidak lebih besar dari variable, system akan print false
+        else{
+            System.out.println(c);
+        }
+    }
+ }
+ */
+
+//output ialah "no sir"
+ //contoh coding untuk logical operator not equal
+/* 
+ public class ComparisonOperator{
+    public static void main(String[] args){
+        int x =5, y =10;
+
+        if (!(x>=5 && y<=10)){
+            System.out.println("yes sir");
+        }
+        else{
+            System.out.println("no sir");
+        }
+    }
+ }
+    */
