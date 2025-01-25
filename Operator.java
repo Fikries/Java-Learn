0f376cebb87c7d96dd8e224cad5efeade340ec32
Fikries <fikries184@gmail.com>
2025-01-25 15:42:02 +0800

    Operator java

---
+++ Operator.java
@@ -0,0 +1,85 @@
+//operator untuk tambah
+/* 
+public class Operator{
+    public static void main(String[] args){
+        int x = 5, y = 10, z = 15;
+        System.out.println(x+y+z);    }
+}
+*/
+//contoh operator tolak 
+//jika nk declare multiple variable need to put like this "int x = 100, y = 760, z = 500;"
+/* 
+public class Operator{
+    public static void main(String[] args){
+        int x = 100, y = 760, z = 500;
+        System.out.println(y-z-x);
+    }
+}
+*/
+
+
+//contoh operator untuk bahagi
+/* 
+public class  Operator{
+    public static void main(String[] args){
+        double x = 100.6, y = 760.6, z =500.6;
+        System.out.println(x/y);
+        System.out.println(z/y);
+    }
+}
+*/
+
+
+//contoh untuki operator darab
+//output ialah 3.8304089816E7
+//E7 ialah 10 pangkat 7 ataus 10,000,000 dimana ada 7 kosong
+/* 
+public class Operator{
+    public static void main(String[] args){
+        double x = 100.6, y = 760.6, z =500.6;
+        System.out.println(x*y*z);
+    }
+}
+    */
+
+
+    //contoh modulus
+    //rumus ialah a / b = c dimana nombor b mesti kecil dari a
+    //contoh ialah 16/3, 3*5=15, 16-15=1
+    // didalam coding ini outputnya ialah 16
+    /* 
+public class Operator{
+    public static void main(String[] args){
+        int x = 100, y = 21;
+        System.out.println(x % y);
+
+    }
+}
+    */
+
+
+//ini contoh untuk increment dimana ++x atau x++ jika declare 2 code in tetap akan membuat increment sebanyak 1 shaja
+//output untuk coding ini ialah 101
+/* 
+public class Operator{
+    public static void main(String[] args){
+        int x =100;
+        ++x;
+        System.out.println(x);
+    }
+}
+*/
+
+
+//contoh coding untuk decrement
+//application sama sahaja seperti increment diatas
+// output coding ini ialah 49
+/* 
+public class Operator{
+    public static void main(String[] args){
+        int x = 50;
+        --x;
+        System.out.println(x);
+    }
+}
+    */
