fdb4339e4d4ff9d1ca3d2375050a7642503a1e9e
Fikries <fikries184@gmail.com>
2025-01-25 14:33:06 +0800

    manualcasting

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+//Manual casting ialah cara untuk buang nombor perpuluhan dibelakang
+//sebagai contoh jika ada 100.12 output akan jadi 100 sahaja
+public class TypeCasting {
+    public static void main(String[] args){
+        double nombor1 = 10.9;
+        nombor1 = (int) nombor1;
+        System.out.println(nombor1);
+    }
+    
+}
+    
+
