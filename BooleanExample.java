c30580dea31436022814029ac61c4f220a0a3fac
Fikries <fikries184@gmail.com>
2025-01-25 14:00:56 +0800

    Boolean Example

---
+++ BooleanExample.java
@@ -0,0 +1,9 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean betul = true;
+        boolean salah = false;
+
+       System.out.println("True ialah " + betul);
+       System.out.println("False ialah " + salah);
+    }
+}
