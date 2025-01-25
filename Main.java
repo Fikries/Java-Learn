9ecace0e69cd53f0b803824a0903f251043974b7
Fikries <fikries184@gmail.com>
2025-01-25 13:34:38 +0800

    java learn

---
+++ Main.java
@@ -0,0 +1,10 @@
+//symbol comment is same with c++
+/*multiline comment is same with c++ also */
+public class Main {
+  public static void main(String[] args){
+  int x = 5, y = 6, z = 50;
+  System.out.println(x + y + z);
+   
+  }
+    }
+  
