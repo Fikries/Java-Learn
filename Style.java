2a4272cde3caaa088fcc0c2d5dae48047825d9dd
Fikries <fikries184@gmail.com>
2025-01-27 10:20:06 +0800

    update

---
+++ Style.java
@@ -0,0 +1,44 @@
+//Contoh untuk string
+//menggunakan function .toUpperCase()
+// maksud () dibawah ialah kita set kan parameter untuk memanggil variavle x iaitu helo world
+/* 
+public class Style {
+    public static void main(String[]args){
+    String x = "HELLO WORLD";
+        System.out.println(x.toUpperCase());
+    
+    }
+}
+*/
+
+
+//ini merupakan contoh untuk concetination
+//kita boleh megunakan ""+"" untuk menggabukan 2 variable dalam satu masa
+//cara kedua ialah dengan munggunakan .concat() 
+//didalam () ,kita boleh set parameter apa-apa sahaja
+//output coding ini ialah=
+/* Hello Nama saya Muhammad fikri bin zaharudin 
+dan saya berumur 23tahun
+Cita- cita saya ingin mengumpul 12.3 Million $
+Pada bulan 8/2025 ini, saya berniat melanjutkan pelajaran dalam bachelor inCybersecurity
+MuhammadFikri*/
+
+/* 
+public class Style{
+    public static void main(String[] args){
+String a = "Hello";
+int b = 23;
+char c = '$';
+double d = 12.3;
+String e = "Cybersecurity";
+String f = "Muhammad";
+String g = "Fikri";
+
+System.out.println(a +" Nama saya Muhammad fikri bin zaharudin ");
+System.out.println("dan saya berumur "+ b +"tahun");
+System.out.println("Cita- cita saya ingin mengumpul " + d + " Million " +c);
+System.out.println("Pada bulan 8/2025 ini, saya berniat melanjutkan pelajaran dalam bachelor in"+ e);
+System.out.println(f.concat( g));
+    }
+}
+    */
