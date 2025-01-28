d0f3323cea034d6bb4293be7ef640759a8d11f69
Fikries <fikries184@gmail.com>
2025-01-28 14:57:04 +0800

    update

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop{
+    public static void main(String[] args){
+        int a = 60;
+        while (a>1){
+            System.out.println(a);
+            a--;
+        }
+    }
+}
---
+++ swithExample.java
@@ -0,0 +1,55 @@
+
+//ini merupakan comtoh basic switch
+//outputnya ialah excellant
+/* 
+public class switchExample {
+    public static void main(String[] args){
+        char op = 'A';
+
+        switch (op){
+            case 'A':
+            System.out.println("excellant");
+            break;
+
+            case 'B':
+            System.out.println("GOOD");
+            break;
+
+            case 'C':
+            System.out.println("FIRE BRUH");
+            break;
+
+            default :
+            System.out.println("That not good");
+
+        }
+    }
+    }
+
+    */
+
+//contoh basic coding menggunakan sudo code
+/* 
+public class swithExample{
+    public static void main(String[] args){
+        int a = 20;
+
+switch (a){
+    case 19:
+        System.out.println("this is value 1");
+        break;
+
+     case 20:
+        System.out.println("this is value 2");
+        break;
+
+    default:
+    System.out.println("this is not value ");
+}
+    }
+}
+    */
+
+
+    
+
